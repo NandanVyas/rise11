85e608bc96eb40b3eec6d2e9daa52d6d2989584c
Nandan Vyas <vyasnandu123@gmail.com>
2023-01-07 15:04:13 +0530

    Created NavBar without css

---
+++ components/NavBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <div>america</div>
+      <div>
+        <ul>
+          <li>book</li>
+          <li>check in</li>
+          <li>manage</li>
+        </ul>
+      </div>
+      <div>
+        <ul><li>Deals</li>
+        <li>Flying with us</li>
+        <li>Where we fly</li>
+        <li>Shop</li>
+        <li>FAQ</li>
+        <li>Fees</li></ul>
+      </div>
+      <div>
+        <div>elevate</div>
+        <button>sign in</button>
+        <button>sign up</button>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
