0ad7d3b32d0f3902cebc639da26682cd3c79e298
Nandan Vyas <vyasnandu123@gmail.com>
2023-01-07 15:47:36 +0530

    Added css to NavBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,28 +1,31 @@
+import Image from "next/image";
 import React from "react";
 
 const NavBar = () => {
   return (
-    <div>
-      <div>america</div>
-      <div>
-        <ul>
+    <div className="flex justify-start h-12 w-full px-6 items-center bg-gradient-to-r from-purple-700 to-red-500 text-white/80">
+      <div className="flex space-x-2 px-6">
+        <Image src="/favicon.ico" alt="logo" width={25} height={25}></Image>
+        <div className="align-text-bottom relative top-1 text-lg">america</div></div>
+      <div className="">
+        <ul className="flex items-center space-x-7 px-6 pr-10 uppercase font-bold text-sm">
           <li>book</li>
           <li>check in</li>
           <li>manage</li>
         </ul>
       </div>
       <div>
-        <ul><li>Deals</li>
+        <ul className="flex items-center space-x-4 px-2 text-xs capitalize font-semibold"><li>Deals</li>
         <li>Flying with us</li>
         <li>Where we fly</li>
         <li>Shop</li>
         <li>FAQ</li>
         <li>Fees</li></ul>
       </div>
-      <div>
-        <div>elevate</div>
-        <button>sign in</button>
-        <button>sign up</button>
+      <div className="flex items-center space-x-2 items-right absolute right-16">
+        <div className="text-lg">elevate</div>
+        <button className="capitalize text-xs px-2 font-semibold">sign in</button>
+        <button className="capitalize text-xs px-2 font-semibold">sign up</button>
       </div>
     </div>
   );
